7e802a812dd60ebd1649de3472a17090e426d6b8
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-06-04 12:28:45 -0500

    added database but not connected yet

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log('May Node be with you')
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+MongoClient.connect('mongodb+srv://mapstonepark:DPYpKjMfxA23T4L9@cluster0.vbv2j.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true})
+    .then(client=> {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+    })
+    .catch(error=> console.error(error))
 
 app.listen(3000, function(){
     console.log('listening on 3000')
@@ -11,5 +22,5 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 app.post('/quotes', (req, res)=> {
-    console.log('Helloooooooooooo!')
+    console.log(req.body)
 })
